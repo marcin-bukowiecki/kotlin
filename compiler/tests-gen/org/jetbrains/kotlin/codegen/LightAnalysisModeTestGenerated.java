6fa95b21ca14a709846642e3d7149230397a2c49
Igor Chevdar <igor.chevdar@jetbrains.com>
2021-04-21 17:25:38 +0500

    [box-tests] Added test

--- compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -11629,6 +11629,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             runTest("compiler/testData/codegen/box/enum/initEntriesInCompanionObject.kt");
         }
 
+        @TestMetadata("initEntriesInCompanionObject2.kt")
+        public void testInitEntriesInCompanionObject2() throws Exception {
+            runTest("compiler/testData/codegen/box/enum/initEntriesInCompanionObject2.kt");
+        }
+
         @TestMetadata("initEntriesInValueOf.kt")
         public void testInitEntriesInValueOf() throws Exception {
             runTest("compiler/testData/codegen/box/enum/initEntriesInValueOf.kt");
